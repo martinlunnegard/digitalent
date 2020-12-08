273225f0b2da0df5d17857745b07245c8479c21d
Martin Lunnegård <martinlunnegard@Martins-iMac.local>
2020-12-08 15:45:24 +0100

    Edit candidate now works

--- src/context/CandidatesReducer.js
+++ src/context/CandidatesReducer.js
@@ -1,7 +1,6 @@
 export default (state, action) => {
   switch(action.type) {
-    case 'ADD_CANDIDATE': 
-    console.log(action.payload, 'payload');
+    case 'ADD_CANDIDATE':
       return {
         ...state,
         candidates: [action.payload, ...state.candidates]
@@ -12,23 +11,20 @@ export default (state, action) => {
         candidates: state.candidates.filter(candidate => candidate.id !== action.payload)
       }
     case 'EDIT_CANDIDATE':
-      return state;
-      // const updatedCandidate = action.payload;
+      const updatedCandidate = action.payload;
+      const updatedCandidates = state.candidates.map(candidate => {
+        if(candidate.id === updatedCandidate.id) {  
+          return updatedCandidate;
+        }
+        return candidate; 
+      });
 
-      // const updatedCandidates = state.candidates.map(candidate => {
-      //   if(candidate.id === updatedCandidate.id) {  
-      //     return updatedCandidate;
-      //   }
-      //   return candidate; 
-      // })
-
-      // return {
-      //   candidates: updatedCandidate  
-      // }
+      return {
+        ...state,
+        candidates: updatedCandidates  
+      }
     
     case 'GET_CANDIDATE':
-    // const editCandidate = state.candidates.filter(candidate => candidate.id === action.payload);
-    // console.log(editCandidate, 'ska vara obj');
       return {
         ...state,
         edit: state.candidates.find(candidate => candidate.id === action.payload)
--- src/pages/EditCandidate.js
+++ src/pages/EditCandidate.js
@@ -1,46 +1,42 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useForm } from 'react-hook-form'; 
-import { Link, useParams, useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { CandidatesContext } from '../context/CandidatesState';
-import Candidate from '../utils/Candidate';
 import * as Constants from '../utils/constants';
 
-const EditCandidate = ({ match }) => {
-  const { id } = useParams();
-  
-  const { getCandidate, editCandidate, edit, cancelEdit } = useContext(CandidatesContext);
-  const { handleSubmit, errors, reset } = useForm();
-  
-  const { register } = useForm({
-    defaultValues: {
-      name: edit.name,
-      age: edit.age,
-      email: edit.email,
-      address: edit.address,
-      stage: edit.stage
-    }
-  });
-  
-   useEffect(() => {
-    if(!id) return;
-    getCandidate(id);
-  }, [id]);
-  
-    // const onSubmit = data => {  
-    //   const { name, age, email, address, stage } = data;
-    //   console.log(data); 
-  
-    //   const newCandidate = new Candidate(name, age, email, address, stage); 
-    //   console.log(newCandidate, 'new candidate');
-  
-  
-    //   addCandidate(newCandidate);
-    //   reset();
-    // }
+const EditCandidate = () => {
+  const { editCandidate, edit, cancelEdit } = useContext(CandidatesContext);
+  const { handleSubmit, errors, reset, register, setValue } = useForm();
+  const history = useHistory();
 
+  useEffect(() => {
+    if(edit) {
+      setValue('id', edit.id);
+      setValue('name', edit.name);
+      setValue('age', edit.age);
+      setValue('email', edit.email);
+      setValue('address', edit.address);
+      setValue('stage', edit.stage);
+    } 
+  },[edit]);
+  
   const onSubmit = data => {
-    console.log(data, 'submitted');
+    const { id, name, age, email, address, stage } = data;
+    
+    const updatedCandidate = { 
+      id,
+      name, 
+      age, 
+      email, 
+      address, 
+      stage
+    }
+
+    editCandidate(updatedCandidate);
+    history.push('/');
   }
+
+  const onError = (errors, e) => console.log(errors, e);
   
   const handleCancel = () => {
     cancelEdit();
@@ -49,25 +45,28 @@ const EditCandidate = ({ match }) => {
   return (
     <section className="candidate-form">
       <h2>Ändra kandidat</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <div>
+          <input name="id" type="hidden" ref={register} />
+        </div>
         <div>
           <label htmlFor="name">Namn: </label>
-          <input name="name" type="text" ref={register({required: 'Fyll i namn'})} />
+          <input name="name" type="text" ref={register} />
         </div>
         {errors.name && <p>{errors.name.message}</p>}
         <div>  
           <label htmlFor="age">Ålder: </label>
-          <input name="age" type="number" ref={register({required: 'Fyll i ålder'})} />
+          <input name="age" type="number" ref={register} />
         </div>
         {errors.name && <p>{errors.age.message}</p>}
         <div>  
           <label htmlFor="email">Email: </label>
-          <input name="email" type="email" ref={register({required: 'Fyll i email'})} />
+          <input name="email" type="email" ref={register} />
         </div>
         {errors.name && <p>{errors.email.message}</p>}
         <div>
           <label htmlFor="address">Adress: </label>
-          <input name="address" type="text" ref={register({required: 'Fyll i adress'})} />
+          <input name="address" type="text" ref={register} />
         </div>
         {errors.name && <p>{errors.address.message}</p>}
         <div>
@@ -80,7 +79,7 @@ const EditCandidate = ({ match }) => {
             <option>{Constants.FINALIZED}</option>
           </select>
         </div>
-        <input type="submit" value="Lägg till" />
+        <input type="submit" value="Klar" />
         <Link to="/" onClick={handleCancel}>Tillbaka</Link>
       </form>
     </section>
